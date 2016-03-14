626460681e6644557089468bc2628866563351e0
Nicholas Sorokin <binarydash@gmail.com>
2016-03-14 19:46:24 +0900

    Add Google Cardboard SDK package.

---
+++ Assets/GyroControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GyroControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Gyroscope gyro = Input.gyro;
+        Vector3 gravity = gyro.gravity;
+
+        Quaternion rotation = Quaternion.FromToRotation(gravity, Vector3.down);
+        transform.rotation = rotation;
+	}
+}
